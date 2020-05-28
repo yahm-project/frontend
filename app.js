25fa33323edef66a9c05a9805f6a1a4b60db8fe6
Daniele <daniele.rossi18@studio.unibo.it>
2020-05-28 17:37:19 +0200

    Refactor fronted. Graphical improvement.

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(cors())
 
 var path = require('path');
 global.appRoot = path.resolve(__dirname);
 
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/www/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/www/index.html');
 });
 
 app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
+    res.status(404).send({ url: req.originalUrl + ' not found' })
 });
 
 let port = 80
-app.listen(port, function () {
-  console.log(`Node server started on port ${port} !`);
+app.listen(port, function() {
+    console.log(`Node server started on port ${port} !`);
 });
